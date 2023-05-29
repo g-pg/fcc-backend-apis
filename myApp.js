e2240fd3e6e53a813c36275bfcbe10cdfe104e32
g-pg <gabrielpgusso@gmail.com>
2023-05-29 16:27:28 -0300

    add middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ const { dirname } = require("path");
 let app = express();
 console.log("Hello World");
 
+app.use(function (req, res, next) {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
