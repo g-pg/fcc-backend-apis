1a7ba77a847ca4fedbe44cf373ead4d10da68489
g-pg <121574011+g-pg@users.noreply.github.com>
2023-05-30 03:33:44 +0000

    add body parser

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+const bodyParser = require('body-parser');
 let express = require("express");
 const { dirname } = require("path");
 let app = express();
 console.log("Hello World");
+
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -25,6 +29,16 @@ app.get('/now', function(req, res, next) {
 }, function(req, res) {
   res.json({ time: req.time })
 })
+
+app.get('/:word/echo', function (req, res) {
+  let word = req.params.word;
+  res.json({echo: word})
+})
+
+app.route('/name').get(function (req, res) {
+  res.json({name: `${req.query.first} ${req.query.last}`})
+})
+
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
