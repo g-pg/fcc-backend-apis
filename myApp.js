ca9b6b3b860607d0b892103f515b9893479a8494
g-pg <121574011+g-pg@users.noreply.github.com>
2023-05-30 00:46:06 +0000

    add middleware function

--- myApp.js
+++ myApp.js
@@ -4,21 +4,27 @@ const { dirname } = require("path");
 let app = express();
 console.log("Hello World");
 
-app.use(function (req, res, next) {
-	console.log(`${req.method} ${req.path} - ${req.ip}`);
-	next();
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
 });
 
-app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/views/index.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json", function (req, res) {
-	let msg = "Hello json";
-	if (process.env.MESSAGE_STYLE === "uppercase") msg = msg.toUpperCase();
-	res.json({ message: msg });
+app.get("/json", function(req, res) {
+  let msg = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") msg = msg.toUpperCase();
+  res.json({ message: msg });
 });
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({ time: req.time })
+})
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
