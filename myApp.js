a755c2e7a7e2dc200ec17bc5cab84acb6a4db540
g-pg <121574011+g-pg@users.noreply.github.com>
2023-05-31 01:46:05 +0000

    complete challenge

--- myApp.js
+++ myApp.js
@@ -1,55 +1,150 @@
 require('dotenv').config();
-
-
-let Person;
+const mongoose = require('mongoose');
+const Person = require('./models/person.js')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const newPerson = new Person({
+    name: 'Teste',
+    age: 17,
+    favoriteFoods: ['abacate', 'cebola']
+  })
+
+  newPerson.save((error, savedPerson) => {
+    if (error) {
+      console.error('Error saving person:', error);
+      return done(error);
+    }
+    console.log('New person saved:', savedPerson);
+    done(null, savedPerson);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (error, createdPeople) => {
+    if (error) {
+      console.error('Error creating people:', error);
+      return done(error);
+    }
+    console.log('People created:', createdPeople);
+    done(null, createdPeople);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (error, peopleFound) => {
+    if (error) {
+      console.error('Error finding people:', error);
+      return done(error);
+    }
+    console.log('People found:', peopleFound);
+    done(null, peopleFound);
+  });
 };
 
+
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function (err, personFound) {
+    if (err) {
+      console.log(err)
+    } else {
+    console.log('found: ', personFound)
+    done(null, personFound);
+    
+    }
+  })
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, found) {
+    if (err) {
+      console.log(err);
+      return done(err);
+    }
+
+    console.log('found: ' + found);
+    return found;
+    return done(null, found)
+  })
+
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (error, person) => {
+    if (error) {
+      console.error('Error finding person:', error);
+      return done(null, error);
+    }
+    person.favoriteFoods.push(foodToAdd);
+    person.save((error, updatedPerson) => {
+      if (error) {
+        console.error('Error saving person:', error);
+        return done(error);
+      }
+      console.log('Person updated:', updatedPerson);
+      return done(null, updatedPerson);
+    });
+  });
 };
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true},
+   function(err, doc) {
+     if (err) {
+       console.log(err)
+       return done(null, err);
+     } else {
+       console.log('edited: '+ doc)
+       return done(null, doc)
+     }
+   })
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function(err, deleted) {
+    if (err) {
+      console.log('error', err)
+      return done(null, err)
+    } else {
+      console.log('deleted', deleted);
+      return done(null, deleted)
+    }
+  })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, removed) => {
+    if (err) {
+      console.log(err);
+      return done(null, err)
+    } else {
+      console.log("removed", removed);
+      return done(null, removed)
+    }
+  })
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((error, data) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        return done(null, error);
+      }
+      console.log('Query result:', data);
+      return done(null, data);
+    });
 };
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
